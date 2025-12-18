f555bf6fb76ef2136df767635f062d4917346139
torjo <faysalhassantorjo8@gmail.com>
2025-12-19 02:04:08 +0600

    feat: Initialize Django project with core configuration, API routing, and a dashboard UI.

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import render
+
+def home(request):
+
+    return render(request, "index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('base.urls')),
+    path("",home)
 ]
